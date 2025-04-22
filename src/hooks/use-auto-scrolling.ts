c8c07a148f6ada5fccf599a0b96dc26db9641b9a
Clemens Akens <clebert@me.com>
2025-04-22 14:00:16 +0200

    Fix auto-scrolling

--- src/hooks/use-auto-scrolling.ts
+++ src/hooks/use-auto-scrolling.ts
@@ -1,21 +1,25 @@
 import { type ReadonlySignal, useComputed } from "@preact/signals";
 import type { RefObject } from "preact";
+import { useRef } from "preact/hooks";
 import { useScrollObserver } from "#hooks/use-scroll-observer.js";
 import { getLastLineBounds } from "#utils/get-last-line-bounds.js";
 
 export function useAutoScrolling(elementRef: RefObject<HTMLElement>): ReadonlySignal<boolean> {
   const $scrollEvent = useScrollObserver();
+  const enabledRef = useRef(true);
 
   return useComputed(() => {
     const scrollEvent = $scrollEvent.value;
 
     if (scrollEvent?.direction === "up") {
-      return false;
+      enabledRef.current = false;
+    } else if (!enabledRef.current) {
+      const lastChild = elementRef.current?.lastChild;
+      const lastLinePosition = lastChild ? getLastLineBounds(lastChild) : undefined;
+
+      enabledRef.current = lastLinePosition?.visible ?? false;
     }
 
-    const lastChild = elementRef.current?.lastChild;
-    const lastLinePosition = lastChild ? getLastLineBounds(lastChild) : undefined;
-
-    return lastLinePosition?.visible ?? false;
+    return enabledRef.current;
   });
 }
